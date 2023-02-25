9f1a093446eb7324f8becaaac1ed486b873144b5
AhmedFarag12 <ahmed.faragg1211@gmail.com>
2023-02-25 05:43:14 +0200

    add app

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let userBox = document.querySelector('.header .header-2 .user-box');
+
+document.querySelector('#user-btn').onclick = () =>{
+   userBox.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .header-2 .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   userBox.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   userBox.classList.remove('active');
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .header-2').classList.add('active');
+   }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+   }
+}
+
+//to top page
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll" , ()=>{
+      if(window.pageYOffset > 100){
+            toTop.classList.add("active")
+      }else{
+            toTop.classList.remove("active")
+   
+      }
+});
+
